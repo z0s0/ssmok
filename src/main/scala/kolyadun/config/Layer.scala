bec86cb5a26ef70be0f9146fe1b2f3d08dd9f01f
vladislav <vlad.kovalenko@aviasales.ru>
2021-03-07 02:02:30 +0700

    move service destinations to config

--- src/main/scala/kolyadun/config/Layer.scala
+++ src/main/scala/kolyadun/config/Layer.scala
@@ -1,19 +1,21 @@
 package kolyadun.config
 
+import kolyadun.model.ServiceDestination
 import pureconfig.ConfigSource
 import pureconfig.generic.auto._
-import zio.{Has, Layer, Task}
+import zio.{Has, Layer, Task, ZLayer}
 
-final case class Config(apiConfig: ApiConfig)
+final case class Config(apiConfig: ApiConfig,
+                        serviceDestinations: List[ServiceDestination])
 final case class ApiConfig(port: Int)
 
 object Layer {
-  type Configs = Has[ApiConfig]
+  type Configs = Has[ApiConfig] with Has[List[ServiceDestination]]
 
   val live: Layer[Throwable, Configs] =
-    Task
-      .effect(ConfigSource.default.loadOrThrow[Config])
-      .map(conf => conf.apiConfig)
-      .toLayer
-
+    ZLayer.fromEffectMany(
+      Task
+        .effect(ConfigSource.default.loadOrThrow[Config])
+        .map(conf => Has(conf.apiConfig) ++ Has(conf.serviceDestinations))
+    )
 }
